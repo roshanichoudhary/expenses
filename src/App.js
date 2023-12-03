2ecd4e79c997c636303e594f28baebc138b0aeb0
Roshani <roshanishete07@gmail.com>
2023-12-03 19:00:25 +0530

    changes in app.js

--- src/App.js
+++ src/App.js
@@ -1,61 +1,52 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import Expenses from './components/Expenses/Expenses';
-import NewExpense from './components/NewExpense/NewExpense';
+import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 const App = () => {
-
   const [expenses, setExpenses] = useState([
     {
-      id: 'e1',
-      title: 'Toilet Paper',
+      id: "e1",
+      title: "Toilet Paper",
       amount: 94.12,
       date: new Date(2020, 7, 14),
     },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
     {
-      id: 'e3',
-      title: 'Car Insurance',
+      id: "e3",
+      title: "Car Insurance",
       amount: 294.67,
       date: new Date(2021, 2, 28),
     },
     {
-      id: 'e4',
-      title: 'New Desk (Wooden)',
+      id: "e4",
+      title: "New Desk (Wooden)",
       amount: 450,
       date: new Date(2021, 5, 12),
     },
-  ])
+  ]);
 
   const addExpenseHandler = (expense) => {
-
     setExpenses((prevState) => {
-
       return [...prevState, expense];
-
-    })
-  }
+    });
+  };
 
   const filterExpenseHandler = (value) => {
-
-
     let newArray = expenses.filter((element) => {
-
       const tempYear = element.date.getFullYear();
       return tempYear == value;
-    })
+    });
 
     setExpenses(newArray);
-  }
-
+  };
 
   return (
     <div>
-      <h2>Let's get started!</h2>
       <NewExpense addExpense={addExpenseHandler} />
       <Expenses items={expenses} filterExpense={filterExpenseHandler} />
     </div>
   );
-}
+};
 
 export default App;
